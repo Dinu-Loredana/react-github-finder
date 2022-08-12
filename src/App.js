6cec8dcce86f9b8c7adcbdf0caa9101993a58844
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-12 16:23:05 +0100

    setup project, tailwind css and daisyui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-purple-100">
+      <h1 className="text-3xl">Hello from App</h1>
+      <button className="btn">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
