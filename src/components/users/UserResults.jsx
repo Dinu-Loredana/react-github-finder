cc3098e96da7d7730c7d325a40bd183e2cbefc49
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-15 10:59:12 +0100

    create UserItem comp to display users list

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Spinner } from "../layout/Spinner";
+import { UserItem } from "./UserItem";
 
 export const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -24,7 +25,7 @@ export const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => {
-          return <h3 key={user.id}>{user.login}</h3>;
+          return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
